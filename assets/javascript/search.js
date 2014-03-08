966c996b3782ade26e2c4fefd62239df9300930e
Matt Mo <matthew.l.mo@gmail.com>
2014-03-07 16:58:03 -0800

    Search - adding search.html, with ajax from dropbox link for xml

---
+++ assets/javascript/search.js
@@ -0,0 +1,23 @@
+// search.js
+
+jQuery(document).ready(function() {
+	jQuery.ajax({
+        type: "GET",
+		url: "https://dl.dropboxusercontent.com/u/883211/nationalPark.xml",
+		dataType: "xml",
+		success: function(xml) {
+			console.log("testing ajax")
+			var searchList = ""
+	 		jQuery(xml).find('name').each(function() {
+	 			searchList += "<dd><span>" + jQuery(this).html() + "</span></dd>";
+	 		});
+	 		// console.log(searchList);
+	 		jQuery('#scroller').html(searchList);
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+			console.log("Error with ajaxing nationalPark.xml")
+			console.log("HTTP status: " + xhr.status);
+			console.log(thrownError);
+		}
+	});
+});
