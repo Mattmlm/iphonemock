0ecd8fb5dd05832021af415a4b88f093bcde1e11
Matt Mo <matthew.l.mo@gmail.com>
2014-03-11 13:53:56 -0700

    Search - adding search bar

--- assets/javascript/header.js
+++ assets/javascript/header.js
@@ -4,4 +4,7 @@ $(document).ready(function() {
 	$('.header-back-button').click(function() {
 		window.history.back();
 	});
+	$('.header-search').click(function() {
+		$('.header-search-bar').toggleClass("active");
+	})
 });
